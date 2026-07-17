aac5a073251d465b096bab6cfeb111e084c22ba9
SchoolingSocial5 <schoolingsocial5@gmail.com>
2026-07-17 10:13:45 +0100

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Deploying Attalis Web..."
+npm install
+npm run build
+
+# Copy static assets to the standalone directory
+cp -r public .next/standalone/
+cp -r .next/static .next/standalone/.next/
+
+# Restart/Start standalone server using PM2
+pm2 restart .next/standalone/server.js --name "attalis-web" || pm2 start .next/standalone/server.js --name "attalis-web"
+
+echo "Attalis Web deployed successfully!"
